45dce60518d9f660a22670a45d985d909970ecc0
Doyles-J <keev005@naver.com>
2025-03-10 14:59:49 +0900

    안쓰는메서드 삭제, 하드코딩메일내용,링크 수정

--- mail/src/main/java/com/kt/mail/service/EmailService.java
+++ mail/src/main/java/com/kt/mail/service/EmailService.java
@@ -38,41 +38,6 @@ public class EmailService {
         }
     }
 
-    public void sendMimeMessage() {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            // 메일을 받을 수신자 설정
-            mimeMessageHelper.setTo("chm2006@naver.com");
-            // 메일의 제목 설정
-            mimeMessageHelper.setSubject("html 적용 테스트 메일 제목");
-            // html 문법 적용한 메일의 내용
-            String content = """
-                    <!DOCTYPE html>
-                    <html xmlns:th="http://www.thymeleaf.org">
-                    <body>
-                    <div style="margin:100px;">
-                    <h1> 테스트 메일 </h1>
-                    <br>
-
-                    <div align="center" style="border:1px solid black;">
-                    <h3> 테스트 메일 내용 </h3>
-                    </div>
-                    <br/>
-                    </div>
-                    </body>
-                    </html>
-                    """;
-            // 메일의 내용 설정
-            mimeMessageHelper.setText(content, true);
-            javaMailSender.send(mimeMessage);
-            log.info("메일 발송 성공!");
-        } catch (Exception e) {
-            log.info("메일 발송 실패!");
-            throw new RuntimeException(e);
-        }
-    }
-
     public void sendEmails(List<Object> recipients, String subject, String body, Integer drillId) {
         try {
             // 디버그: 수신자 목록과 타입 출력
@@ -125,7 +90,7 @@ public class EmailService {
                     String trackingLink = String.format("https://localhost:8080/track/%d/%s", 
                         drillId, email.hashCode());
                     String fullBody = (body != null ? body : "테스트 내용") + 
-                        "\n\n추적 링크: " + trackingLink;
+                        "\n\n " + trackingLink;
                     
                     message.setText(fullBody);
                     javaMailSender.send(message);
